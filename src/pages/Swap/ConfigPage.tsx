132b186e624f838ce8c5ea4b4728122e1cc5945e
TomL94 <langer.tom7@gmail.com>
2021-02-02 14:13:29 +0200

    Slippage input semi-working

--- src/pages/Swap/ConfigPage.tsx
+++ src/pages/Swap/ConfigPage.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Button, Input, Grid, Search } from 'semantic-ui-react';
+import { Button, Input, Grid, Search, Radio } from 'semantic-ui-react';
 import cn from 'classnames';
 import * as styles from './styles/styles.styl';
-import { Dictionary } from 'lodash';
 
 export class ConfigPage extends React.Component<{}, { slippage: number }> {
   state = { slippage: 0.5 };
@@ -15,37 +14,44 @@ export class ConfigPage extends React.Component<{}, { slippage: number }> {
       <>
         <div>Transaction Settings</div>
         <div>Slippage tolerance</div>
-        <Button
-          basic={slippage !== 0.1}
-          onClick={() => this.handleClick(0.1)}
-          compact
-          circular
-          size="small"
-          color="blue"
-        >
-          0.1%
-        </Button>
-        <Button
-          basic={slippage !== 0.5}
-          onClick={() => this.handleClick(0.5)}
-          compact
-          circular
-          size="small"
-          color="blue"
-        >
-          0.5%
-        </Button>
-        <Button
-          basic={slippage !== 1}
-          onClick={() => this.handleClick(1)}
-          compact
-          circular
-          size="small"
-          color="blue"
-        >
-          1%
-        </Button>
-        {/* <Input placeholder="0.5%" size="small" icon={'none'} /> */}
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <Button
+            basic={slippage !== 0.1}
+            onClick={() => this.handleClick(0.1)}
+            compact
+            circular
+            size="small"
+            color="blue"
+          >
+            0.1%
+          </Button>
+          <Button
+            basic={slippage !== 0.5}
+            onClick={() => this.handleClick(0.5)}
+            compact
+            circular
+            size="small"
+            color="blue"
+          >
+            0.5%
+          </Button>
+          <Button
+            basic={slippage !== 1}
+            onClick={() => this.handleClick(1)}
+            compact
+            circular
+            size="small"
+            color="blue"
+          >
+            1%
+          </Button>
+          <Input placeholder="0.5%" size="small" icon={'none'}>
+            <input className={cn(styles.slippageInput)}></input>
+          </Input>
+        </div>
+        <div>
+          Toggle Expert Mode <Radio toggle />
+        </div>
       </>
     );
   }
