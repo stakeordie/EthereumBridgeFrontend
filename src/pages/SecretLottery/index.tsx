a8dd3b940f2dcb4de4e6fd123467f23b523e3989
jsandovalg <jorge@stakeordie.com>
2021-08-09 10:07:55 -0700

    add secret lottery route and initialize secret lottery component

---
+++ src/pages/SecretLottery/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.scss';
+
+const SecretLottery = () => {
+    return (
+        <>
+            <h1>Hello From Secret Lottery</h1>
+        </>
+    )
+}
+
+export default SecretLottery;
