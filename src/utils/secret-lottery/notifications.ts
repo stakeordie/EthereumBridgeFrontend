863e298797bacdc422c5ceaa2445cf521bdd2e10
jsandovalg <jorge@stakeordie.com>
2021-09-02 16:27:47 -0700

    update and integrate notifications for modal

--- src/utils/secret-lottery/notifications.ts
+++ src/utils/secret-lottery/notifications.ts
@@ -1,26 +1,33 @@
 import { notify } from "components/Lottery/util";
+import { notifyTop } from '../../components/Lottery/util';
 import { store } from "react-notifications-component";
 
-export const errorNotification = async (error: Error) => {
-  notify('error',error.message);
+export const errorNotification = async (error: Error, isTop?: boolean) => {
+  
+  if (isTop) {
+    notifyTop('error', error.message);
+  } else {
+    notify('error', error.message);
+  }
   // store.addNotification({
   //   title: "Error",
   //   message: error.message,
   //   type: "danger",
-  //   insert: "bottom",
-  //   container: "bottom-left",
+  //   insert: "top",
+  //   container: "top-center",
   //   animationIn: ["animate__animated", "animate__fadeIn"],
   //   animationOut: ["animate__animated", "animate__fadeOut"],
-  //   dismiss: {
-  //     duration: 3000,
-  //     onScreen: true,
-  //   },
   // });
   return null;
 }
 
-export const successNotification = async (message: string) => {
-  notify('success',message);
+export const successNotification = async (message: string, isTop?: boolean) => {
+
+  if (isTop) {
+    notifyTop('success', message);
+  } else {
+    notify('success', message);
+  }
   // store.addNotification({
   //   title: "Success",
   //   message: message,
