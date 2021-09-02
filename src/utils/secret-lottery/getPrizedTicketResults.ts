08b2150d506c65cf5480c0ef4370df0803cb3a7b
David SoD <david@stakeordie.com>
2021-09-02 16:58:54 -0700

    Try fix random bug in My tickets modal

--- src/utils/secret-lottery/getPrizedTicketResults.ts
+++ src/utils/secret-lottery/getPrizedTicketResults.ts
@@ -1,3 +1,4 @@
+import { result } from "lodash";
 import { IUserTicket } from "../../pages/SecretLottery/api/getUserRoundPaginatedTickets"
 
 export default (draftedTicket: string, tickets: IUserTicket[]) => {
@@ -9,15 +10,21 @@ export default (draftedTicket: string, tickets: IUserTicket[]) => {
         sequence_5: [],
         sequence_6: [],
     }
-    
-    for (let ticket of tickets) {
-        if (ticket.ticket.substring(0, 1) === draftedTicket.substring(0, 1)) results.sequence_1.push(ticket)
-        if (ticket.ticket.substring(0, 2) === draftedTicket.substring(0, 2)) results.sequence_2.push(ticket)
-        if (ticket.ticket.substring(0, 3) === draftedTicket.substring(0, 3)) results.sequence_3.push(ticket)
-        if (ticket.ticket.substring(0, 4) === draftedTicket.substring(0, 4)) results.sequence_4.push(ticket)
-        if (ticket.ticket.substring(0, 5) === draftedTicket.substring(0, 5)) results.sequence_5.push(ticket)
-        if (ticket.ticket.substring(0, 6) === draftedTicket.substring(0, 6)) results.sequence_6.push(ticket)
+    if(tickets.length == 0) return results;
+    try {
+      for (let ticket of tickets) {
+          if (ticket.ticket.substring(0, 1) === draftedTicket.substring(0, 1)) results.sequence_1.push(ticket)
+          if (ticket.ticket.substring(0, 2) === draftedTicket.substring(0, 2)) results.sequence_2.push(ticket)
+          if (ticket.ticket.substring(0, 3) === draftedTicket.substring(0, 3)) results.sequence_3.push(ticket)
+          if (ticket.ticket.substring(0, 4) === draftedTicket.substring(0, 4)) results.sequence_4.push(ticket)
+          if (ticket.ticket.substring(0, 5) === draftedTicket.substring(0, 5)) results.sequence_5.push(ticket)
+          if (ticket.ticket.substring(0, 6) === draftedTicket.substring(0, 6)) results.sequence_6.push(ticket)
+      }
+      
+    } catch (error) {
+      return results;
     }
+
 
     return results
 }
