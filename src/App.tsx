376cae53ac0edb59e0d4ad8fffdddb4ae7fee578
Walter Hurtado <walter.hurtado@lkmx.io>
2021-09-13 19:46:10 -0700

    - Website under maintenance page

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import { SwapPagePool } from './pages/Pool';
 import { InfoModal } from './components/InfoModal';
 import { EarnRewards } from './pages/Earn';
 import { FAQPage } from './pages/FAQ';
+import { Maintenance } from './pages/Maintenance';
 import { FinancePage } from './pages/Finance';
 import { SeFiPage } from './pages/SeFi';
 import { Cashback } from './pages/Cashback'
@@ -20,6 +21,26 @@ import CreateProposal from 'pages/CreateProposal';
 import { DetailProposal } from 'pages/DetailProposal';
 import SefiStaking from 'pages/SefiStaking';
 import { Migration } from 'pages/Migration';
+
+const hideEverything = function() {
+  const bodyEl = document.querySelector('body');
+  const alertbarEl = document.querySelector('.messsage-body');
+  const menuHeaderEl = document.querySelector('.nav_menu__items');
+  const menuRightEl = document.querySelector('.menu-right');
+  const footerEl1 = document.querySelector('._3e3M01KJ95etCclEH4wYSU');
+  const footerEl2 = document.querySelector('._3YJ6kqOrkmuAGe3uG6Z8fl');
+  bodyEl.setAttribute("style", "opacity: 0;");
+  footerEl1.setAttribute("style", "opacity: 0;");
+  footerEl2.setAttribute("style", "opacity: 0;");
+  alertbarEl.setAttribute("style", "opacity: 0;");
+  menuHeaderEl.setAttribute("style", "opacity: .3; pointer-events: none;");
+  menuRightEl.setAttribute("style", "opacity: .3; pointer-events: none;");
+  bodyEl.setAttribute("style", "opacity: 1;");
+};
+
+setTimeout(() => {
+  hideEverything();
+}, 200);
 
 export const App: React.FC = () => (
   <Providers>
@@ -34,6 +55,7 @@ export const App: React.FC = () => (
         <Route exact path="/proposal/:id" component={DetailProposal} />
         <Route exact path="/sefistaking" component={SefiStaking} />
         <Route exact path="/migration" component={Migration} />
+        <Route exact path="/Maintenance" component={Maintenance} />
         {/* <Route exact path="/faq" component={FAQPage} /> */}
         {/* <Route exact path="/finance" component={FinancePage} /> */}
         {/* <Route exact path="/info" component={InfoPage} /> */}
@@ -41,7 +63,7 @@ export const App: React.FC = () => (
         {/* <Route exact path="/earn" component={EarnRewards} /> */}
         {/* <Route exact path="/:token" component={EthBridge} /> */}
         <Route exact path="/:token/operations/:operationId" component={EthBridge} />
-        <Redirect to="/swap" />
+        <Redirect to="/maintenance" />
       </Switch>
     </Suspense>
     <ActionModals />
