de4c14f66f6157cb1942a9c29fe8e8a197fdea3d
David Rodríguez <david.rodriguez@lkmx.io>
2021-10-15 18:30:43 -0700

    Adjust gas fee in lottery claim

--- src/pages/SecretLottery/api/claimRewards.ts
+++ src/pages/SecretLottery/api/claimRewards.ts
@@ -1,34 +1,45 @@
-import { TxsResponse } from "secretjs";
-import { IClientState } from "../../../stores/lottery-context/ClientContext";
-import entropy from "../../../utils/secret-lottery/entropy";
-const { fromUtf8 } = require("@iov/encoding");
+import { TxsResponse } from 'secretjs';
+import { StdFee } from 'secretjs/types/types';
+import { IClientState } from '../../../stores/lottery-context/ClientContext';
+import entropy from '../../../utils/secret-lottery/entropy';
+const { fromUtf8 } = require('@iov/encoding');
 
-export default async (client: IClientState, contractAddress: string,  round: number, tickets_index: number[]) => {
-    let handleMsg = {
-        claim_rewards: {
-            round,
-            tickets_index
-        }
-    };
+export default async (client: IClientState, contractAddress: string, round: number, tickets_index: number[]) => {
+  let handleMsg = {
+    claim_rewards: {
+      round,
+      tickets_index,
+    },
+  };
+  const calculatedFee = (8000 * tickets_index.length + 300000).toString();
+  const fee: StdFee = {
+    amount: [{ denom: 'uscrt', amount: calculatedFee }],
+    gas: calculatedFee,
+  };
+  const { transactionHash } = await client.execute.execute(
+    contractAddress,
+    handleMsg,
+    'Claiming rewards for lottery',
+    [],
+    fee,
+  );
 
-    const { transactionHash } = await client.execute.execute(contractAddress, handleMsg); 
+  const tx: TxsResponse = await new Promise((accept, reject) => {
+    const interval = setInterval(async () => {
+      try {
+        //@ts-ignore
+        const tx = await client.execute.restClient.txById(transactionHash, false);
+        accept(tx);
+        clearInterval(interval);
+      } catch (error) {
+        //console.error(error);
+      }
+    }, 2000);
+  });
 
-    const tx:TxsResponse = await new Promise((accept, reject) => {
-        const interval = setInterval(async () => {
-          try {
-            //@ts-ignore
-            const tx = await client.execute.restClient.txById(transactionHash,false);
-            accept(tx);
-            clearInterval(interval);
-          } catch (error) {
-            //console.error(error);
-          }
-        }, 2000);
-    });
-
-    if (tx.data.length > 0) {
-        return true
-    } else {
-        throw Error(tx.raw_log)
-    }
-} 
+  if (tx.data.length > 0) {
+    return true;
+  } else {
+    throw Error(tx.raw_log);
+  }
+};
